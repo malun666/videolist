fbf94e5cac8c8d225c0263b33ed9befdd79f30e6
malun <malun666@126.com>
2018-08-13 19:28:24 +0800

    :art: ss

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.engine('art', art_express);
 var options = {
   dotfiles: 'ignore',
   etag: false,
-  extensions: ['htm', 'html'],
+  extensions: [
+    'htm', 'html'
+  ],
   index: false,
   maxAge: '1d',
   redirect: false,
@@ -23,17 +25,59 @@ var options = {
 
 app.use(express.static('public', options));
 
-app.use('/', (req, res) => {
- fs.readdir(path.join(__dirname, '/public/video'), (err, files) => {
-  let filesArray = [];
-  files.forEach((item) => {
-    if(item == '.DS_Store') {
-      return;
+
+
+app.use('/listarry', (req, res) => {
+  // return res.json({age: 1});
+  fs.readdir(path.join(__dirname, '/public/video'), (err, files) => {
+    let data = files.map((item) => {
+      return getItemData(item,'/public/video');
+      // if (item == '.DS_Store') {
+      //   return;
+      // }
+      // if(fs.statSync(item).isDirectory()) {
+
+      // }
+      // filesArray.push(path.join('/video', item));
+    });
+    res.json(data.filter(item => item));
+  })
+});
+
+function getItemData(item, purl, pId = -1) {
+  if (item == '.DS_Store') {
+    return null;
+  }
+  let id = Date.now();
+  if(fs.statSync(path.join(__dirname,purl, item)).isDirectory()) {
+    // fs.readdirSync()
+    // getItemData(, id)
+    let dirFiles = fs.readdirSync(path.join(__dirname,purl, item));
+    let childFiles = dirFiles.map(child => {
+      return getItemData(child, purl + '/' + item, id);
+    });
+
+    return {
+      isFile: false,
+      id,
+      text: item,
+      name: path.join(purl.replace('/public', ''), item),
+      children: childFiles,
+      title: item
     }
-    filesArray.push(path.join('/video', item));
-  });   
-  res.render('index.art', {files: filesArray});
- });
+  }
+  return {
+    name: path.join(purl.replace('/public', ''), item),
+    text: item,
+    id: id,
+    isFile: true,
+    title: item,
+    pId
+  }
+}
+
+app.use('/', (req, res) => {
+  res.render('index.art');
 });
 
 app.listen(3000, function () {
